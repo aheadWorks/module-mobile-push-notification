d78d4b5a948dfd62d3e5a22e73cf4ad360dc9e77
Nimish Pawar <pawar@ravedigital.agency>
2022-12-02 19:04:03 +0530

    MAC-6 Add check module file

---
+++ Model/Checkmodule.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Aheadworks\MobilePushNotification\Model;
+
+use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Module\Manager;
+
+/**
+ * Class Checkmodule
+ * @package Aheadworks\MobilePushNotification\Model
+ */
+class Checkmodule extends AbstractModel
+{
+    /**
+     * @var Manager
+     */
+    private $moduleManager;
+
+    /**
+     * @param Manager $moduleManager
+     */
+    public function __construct(
+        Manager $moduleManager
+    ) {
+        $this->moduleManager = $moduleManager;
+    }
+
+    public function checkModule()
+    {
+    	if ($this->moduleManager->isEnabled('Aheadworks_MobilePushNotification')) {
+		   return true;
+		} else {
+		   return false;
+		}
+    }
+}
