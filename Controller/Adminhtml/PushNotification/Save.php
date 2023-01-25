51af274ba92da023f5a0a83f30934650b5150981
Nimish Pawar <pawar@ravedigital.agency>
2023-01-25 12:34:03 +0530

    MAC-6 Update save file

--- Controller/Adminhtml/PushNotification/Save.php
+++ Controller/Adminhtml/PushNotification/Save.php
@@ -9,6 +9,7 @@ use Aheadworks\MobilePushNotification\Model\Upload\Info;
 use Magento\Framework\App\Action\Action;
 use Aheadworks\MobilePushNotification\Model\Upload\UploaderPath;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\Controller\ResultFactory;
 
 /**
  * Save push notification data
@@ -71,26 +72,24 @@ class Save extends Action
      */
     public function execute()
     {
-        $resultRedirect = $this->resultRedirectFactory->create();
-        $resultRedirect->setUrl($this->_redirect->getRefererUrl()); // phpcs:ignore
         $data = $this->getRequest()->getPostValue();
         
         if (!empty($data['message_title']) && !empty($data['message'])) {
             $messageTitle = $data['message_title'];
             $message = $data['message'];
-
             if (!empty($data['notification_image'])) {
                 if (isset($data['notification_image']['0']['path'])) {
                     $imgName = $data['notification_image']['0']['name'];
                     $data['notification_image'] = $this->uploaderPath->getPathName().'/'.$imgName;
                     $notificationImageUrl = $this->infoImage->getMediaUrl($imgName);
-                } else {
+                } elseif (isset($data['notification_image']['0']['url'])) {
                     $imgName = $data['notification_image']['0']['url'];
                     $data['notification_image'] = $imgName;
                     $currentUrl = $this->storeManager->getStore()
                     ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
-                    $newcurrentUrl = rtrim($currentUrl, '/');
-                    $notificationImageUrl = $newcurrentUrl.$imgName;
+                    $notificationImageUrl = $currentUrl.$imgName;
+                } else {
+                    $notificationImageUrl = '';
                 }
             } else {
                 $notificationImageUrl = '';
@@ -114,7 +113,8 @@ class Save extends Action
         } else {
             $this->messageManager->addErrorMessage(__('Something went wrong while sending the notification'));
         }
-        
-        return $resultRedirect;
+
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
     }
 }
